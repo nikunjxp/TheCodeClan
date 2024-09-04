d41fe4d99fa172af5d4534f1578214e35721fd42
Nikunj Paliwal <152013638+nikunjxp@users.noreply.github.com>
2024-09-04 22:33:36 +0530

    Add files via upload

---
+++ components/Footer.js
@@ -0,0 +1,50 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={styles.wrapper}>
+      <p className={styles.text}>
+        Made with 💖 by{" "}
+        <a
+          href="https://github.com/madzadev"
+          target="_blank"
+          rel="noreferrer"
+          className={styles.link}
+        >
+          TheCodeClan
+        </a>
+      </p>
+      <p className={styles.text}>
+        Powered by{" "}
+        <a
+          href="https://nextjs.org"
+          target="_blank"
+          rel="noreferrer"
+          className={styles.link}
+        >
+          NextJS
+        </a>
+        ,{" "}
+        <a
+          href="https://npmjs.com/package/react-cropper"
+          target="_blank"
+          rel="noreferrer"
+          className={styles.link}
+        >
+          React-cropper
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://netlify.com"
+          target="_blank"
+          rel="noreferrer"
+          className={styles.link}
+        >
+          Netlify
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ data/about.js
@@ -0,0 +1,29 @@
+export const about = [
+  {
+    question: "What is CropScore?",
+    answers: [
+      "CropScore is an online image cropper designed for content creators.",
+      "It calculates the score of how well the real-time crop will fit for the target use.",
+      "Custom resolutions and aspect ratios are also supported.",
+    ],
+    image: "/graphics/about/scissors.png",
+  },
+  {
+    question: "Why was it built?",
+    answers: [
+      "The project was built for hackathon.",
+    ],
+    image: "/graphics/about/idea.png",
+  },
+  {
+    question: "How was it built?",
+    answers: [
+      "The project is built with NextJS and react-cropper.",
+      "Chakra UI is used for components.",
+      "The icons are taken from Flaticon and React icons library.",
+
+    ],
+    image: "/graphics/about/build.png",
+  },
+  
+];
