2bae9ae04bfebe3693e8bceebc6727686eb1192e
nikunjxp <np1354@srmist.edu.in>
2024-09-05 11:18:40 +0530

    HTML

---
+++ cropper_files/252f366e-4889826a29a38271.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[228],{5434:function(t,n,a){a.d(n,{Ap8:function(){return e}});var h=a(8357);function e(t){return(0,h.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M5 20h14v-2H5v2zm0-10h4v6h6v-6h4l-7-7-7 7z"}}]})(t)}}}]);
